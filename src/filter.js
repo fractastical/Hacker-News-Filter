203a74b1b00bf7b01924213124c1fe7fc4360b45
Jared <pretender314@gmail.com>
2012-03-18 21:12:40 -0500

    linting and formating

--- src/filter.js
+++ src/filter.js
@@ -1,27 +1,22 @@
 //TODO: Crazy namespacing here...
-chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"},
-
-	 function(response) {
-	   
-	 	   var filters = JSON.parse(response.data);
-		   if(!filters["default"])
-				filters["default"] = 1;
-		
-			filterDocument(0,filters);
-		
-	});
+chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"}, function(response) {
+	var filters = JSON.parse(response.data);
+	if(!filters["default"])
+	filters["default"] = 1;
+	filterDocument(0,filters);
+});
 	
 /*
 	params:
-	 	doc: document for items to be appended to
-		data: data to be used 
+		doc: document for items to be appended to
+		data: data to be used
 		counter: number of times performed
-	
 */
-	function filterDocument(counter,filters) {	
+	function filterDocument(counter,filters) {
 		counter++;
 		var tds = document.getElementsByTagName('td');
-		var tdsToRemove = Array();
+		var tdsToRemove = [];
+
 		for(var index = 0; index < tds.length; index++)
 		{
 
@@ -30,40 +25,42 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"},
 			var nextRowSet;
 
 			if (td.getAttribute('class') &&
-				td.getAttribute('class').indexOf('title') != -1 && 
-				td.firstChild && 
+				td.getAttribute('class').indexOf('title') != -1 &&
+				td.firstChild &&
 				td.firstChild.tagName == 'A')
-			{		
+			{
 				if(td.parentNode &&
-				   td.parentNode.nextSibling &&
-				   td.parentNode.nextSibling.firstChild &&
-				   td.parentNode.nextSibling.firstChild.nextSibling &&
-				   td.parentNode.nextSibling.firstChild.nextSibling.getAttribute('class') &&
-				   td.parentNode.nextSibling.firstChild.nextSibling.getAttribute('class').indexOf('subtext') != -1 &&
-			       td.parentNode.nextSibling.firstChild.nextSibling.firstChild &&
-			       td.parentNode.nextSibling.firstChild.nextSibling.firstChild.tagName == 'SPAN' &&
-			       td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText &&
-			       td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText.indexOf(' ') != -1 )
-					  score = parseInt(td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText.split(' ')[0]);
+					td.parentNode.nextSibling &&
+					td.parentNode.nextSibling.firstChild &&
+					td.parentNode.nextSibling.firstChild.nextSibling &&
+					td.parentNode.nextSibling.firstChild.nextSibling.getAttribute('class') &&
+					td.parentNode.nextSibling.firstChild.nextSibling.getAttribute('class').indexOf('subtext') != -1 &&
+					td.parentNode.nextSibling.firstChild.nextSibling.firstChild &&
+					td.parentNode.nextSibling.firstChild.nextSibling.firstChild.tagName == 'SPAN' &&
+					td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText &&
+					td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText.indexOf(' ') != -1 ) {
+						score = parseInt(td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText.split(' ')[0]);
+				}
 
-				if(score == 0 && td.firstChild.text == "More")
+				if(score === 0 && td.firstChild.text == "More")
 				{
 					var tablebody = td.parentNode.parentNode;
 					$(td).remove();
 					jQuery.get(td.firstChild.href, function(data) {
-						
 						$(data).find('table table tr').slice(4).appendTo(tablebody);
-						if(counter < 8)
-						  	filterDocument(counter,filters);						
-					})		
+						if(counter < 8) {
+							filterDocument(counter,filters);
+						}
+					});
 				
 				}
 			
-				if(score != 0 && hn_filter_match(td.firstChild.text, score, filters)) 
+				if(score !== 0 && hn_filter_match(td.firstChild.text, score, filters))
 					tdsToRemove.push(td);
 			}
 		}
-		for (x in tdsToRemove) 
+
+		for (x in tdsToRemove)
 		{
 			console.log('h');
 			var td = tdsToRemove[x];
@@ -73,14 +70,14 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"},
 
 			tr1.parentNode.removeChild(tr1);
 			tr2.parentNode.removeChild(tr2);
-			tr3.parentNode.removeChild(tr3);	
+			tr3.parentNode.removeChild(tr3);
 		}
 
 
 	}
 
 	function hn_filter_match(text, score, filters)
-	{		
+	{
 		
 			var remove = false;
 		
