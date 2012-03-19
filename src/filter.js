ed0bc0b4412df98cf46d4aa6c4556a9ebaff28a0
Jared <pretender314@gmail.com>
2012-03-19 18:33:47 -0500

    went to long without commiting

--- src/filter.js
+++ src/filter.js
@@ -1,33 +1,73 @@
 //TODO: Crazy namespacing here...
 chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"}, function(response) {
-	var filters = response.data ? JSON.parse(response.data) : { "default" : 1 };
-	if(!filters["default"])
-		filters["default"] = 1;
-	filterDocument(0,filters);
+	var options = response.data ? JSON.parse(response.data) : { "default" : 1 };
+	if(!options["default"])
+		options["default"] = 1;
+	filterDocument(options);
 });
-	
+
+
+	NodeList.prototype.map = function(m){
+		var result = [];
+		for (var i = 0, l = this.length; i < l; i++) {
+			result.push(m(this[i]));
+		}
+		return result;
+	};
 /*
 	params:
 		doc: document for items to be appended to
 		data: data to be used
-		counter: number of times performed
 */
-	function filterDocument(counter,filters) {
-		counter++;
+	function filterDocument(filters) {
+		expand_more_link(
+			function(d){ return d.querySelector('a[href^="/x?fnid"]'); },
+			5
+			);
+				
 		var tds = document.querySelectorAll('td[class="title"]');
-		//document.querySelector('a[href^="/x?fnid"]')
+		var rows = document.querySelectorAll('td[class="title"]')
+			.map(function(node){ return node.parentNode; });
+		
+		
 
-		for(var index = 0; index < tds.length; index++)
+		for(var i = 0, l = tds.length; i < l; i++)
 		{
+			var td = tds[i];
 
-			var td = tds[index];
-			var score = 0;
-			var nextRowSet;
-
-			if (td.firstChild &&
-				td.firstChild.tagName == 'A')
+			if (td.firstChild && td.firstChild.tagName == 'A')
 			{
-				if(td.parentNode &&
+				var score = get_headline_score(td);
+				
+				if(score !== 0 && hn_filter_match(td.firstChild.text, score, filters)) {
+					remove_headline(td);
+				}
+			}
+		}
+	}
+
+
+	function expand_more_link(search, count){
+		if(count < 1) return;
+
+		/*
+		if(score === 0 && td.firstChild.text == "More")
+		{
+			var tablebody = td.parentNode.parentNode;
+			$(td).remove();
+			jQuery.get(td.firstChild.href, function(data) {
+				$(data).find('table table tr').slice(4).appendTo(tablebody);
+				if(counter < 8) {
+					filterDocument(counter,filters);
+				}
+			});
+		
+		}
+		*/
+	}
+
+	function get_headline_score(td) {
+		if(td.parentNode &&
 					td.parentNode.nextSibling &&
 					td.parentNode.nextSibling.firstChild &&
 					td.parentNode.nextSibling.firstChild.nextSibling &&
@@ -37,30 +77,10 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"}, f
 					td.parentNode.nextSibling.firstChild.nextSibling.firstChild.tagName == 'SPAN' &&
 					td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText &&
 					td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText.indexOf(' ') != -1 )
-				{
-						score = parseInt(td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText.split(' ')[0]);
-				}
-
-				/*
-				if(score === 0 && td.firstChild.text == "More")
-				{
-					var tablebody = td.parentNode.parentNode;
-					$(td).remove();
-					jQuery.get(td.firstChild.href, function(data) {
-						$(data).find('table table tr').slice(4).appendTo(tablebody);
-						if(counter < 8) {
-							filterDocument(counter,filters);
-						}
-					});
-				
-				}
-				*/
-			
-				if(score !== 0 && hn_filter_match(td.firstChild.text, score, filters)) {
-					remove_headline(td);
-				}
-			}
+		{
+				return parseInt(td.parentNode.nextSibling.firstChild.nextSibling.firstChild.innerText.split(' ')[0]);
 		}
+		return 0;
 	}
 
 	function remove_headline(td) {
@@ -74,8 +94,9 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"}, f
 	}
 
 	function hn_filter_match(text, score, filters) {
-			var remove = score < filters["default"];
 
+			var remove = score < filters["default"];
+			console.log('score:' + score + ' defaults:' + filters['default']);
 			var t = text.toLowerCase();
 
 			for (var x in filters) {
@@ -86,4 +107,3 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"}, f
 
 			return remove;
 	}
-
