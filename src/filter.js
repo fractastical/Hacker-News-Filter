837e9da3ab3d51da7973011c66f52b6751b0e886
Jared <pretender314@gmail.com>
2012-03-19 19:56:38 -0500

    removing debug logging and fixing spacing

--- src/filter.js
+++ src/filter.js
@@ -11,84 +11,76 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "filterValues"}, f
 			);
 });
 
-/*
-	params:
-		doc: document for items to be appended to
-		data: data to be used
-*/
-	function filterDocument(defaultmin, filters) {
-		// Follows the More links, adds the results to the table, and deletes them
-		
-		
-		var links = document.querySelectorAll('td[class="title"] a');
 
-		for(var i = 0, l = links.length; i < l; i++)
-		{
-			var link = links[i];
+function filterDocument(defaultmin, filters) {
+	
+	var links = document.querySelectorAll('td[class="title"] a');
 
-			var score = get_headline_score(link);
-			if(score && hn_filter_match(link.textContent, score, filters, defaultmin)) {
-				remove_headline(link);
-			}
+	for(var i = 0, l = links.length; i < l; i++)
+	{
+		var link = links[i];
+
+		var score = get_headline_score(link);
+		if(score && hn_filter_match(link.textContent, score, filters, defaultmin)) {
+			remove_headline(link);
+		}
+	}
+}
+
+
+function expand_more_links(search, count, success){
+	if(count < 1){
+		success();
+		return;
+	}
+
+	var link = search(document);
+	if(!link) return;
+
+	var href = link.href;
+	var table = link.parentNode.parentNode.parentNode.parentNode;
+	
+	// Remove link and spacer
+	var row = link.parentNode.parentNode;
+	var parent = link.parentNode.parentNode.parentNode;
+	parent.removeChild(row.previousSibling);
+	parent.removeChild(row);
+
+	// Follow more link and add results to the table
+	jQuery.get(href, function(data) {
+		table.appendChild($(data).find('table table tbody')[1]);
+		expand_more_links(search, --count, success);
+	});
+}
+
+function get_headline_score(link) {
+	var subtext = link.parentNode.parentNode.nextSibling;
+	if(!subtext) return 0;
+	var scoretext = subtext.querySelector('span[id^="score"]');
+	if(!scoretext) return 0;
+
+	return parseInt(scoretext.textContent.split(' ')[0], 10);
+}
+
+function remove_headline(link) {
+	var headline = link.parentNode.parentNode;
+	var parent = headline.parentNode;
+	
+	parent.removeChild(headline.nextSibling.nextSibling);
+	parent.removeChild(headline.nextSibling);
+	parent.removeChild(headline);
+}
+
+function hn_filter_match(text, score, filters, defaultmin) {
+	var remove = score < defaultmin;
+
+	var t = text.toLowerCase();
+
+	for (var x in filters) {
+		if(t.match(x)) {
+			remove = score < filters[x];
 		}
 	}
 
-
-	function expand_more_links(search, count, success){
-		if(count < 1){
-			success();
-			return;
-		}
-
-		var link = search(document);
-		if(!link) return;
-
-		var href = link.href;
-		var table = link.parentNode.parentNode.parentNode.parentNode;
-		
-		// Remove link and spacer
-		var row = link.parentNode.parentNode;
-		var parent = link.parentNode.parentNode.parentNode;
-		parent.removeChild(row.previousSibling);
-		parent.removeChild(row);
-
-		// Follow more link and add results to the table
-		jQuery.get(href, function(data) {
-			table.appendChild($(data).find('table table tbody')[1]);
-			expand_more_links(search, --count, success);
-		});
-	}
-
-	function get_headline_score(link) {
-		var subtext = link.parentNode.parentNode.nextSibling;
-		if(!subtext) return 0;
-		var scoretext = subtext.querySelector('span[id^="score"]');
-		if(!scoretext) return 0;
-
-		return parseInt(scoretext.textContent.split(' ')[0], 10);
-	}
-
-	function remove_headline(link) {
-		var headline = link.parentNode.parentNode;
-		var parent = headline.parentNode;
-		
-		parent.removeChild(headline.nextSibling.nextSibling);
-		parent.removeChild(headline.nextSibling);
-		parent.removeChild(headline);
-	}
-
-	function hn_filter_match(text, score, filters, defaultmin) {
-
-			var remove = score < defaultmin;
-
-			var t = text.toLowerCase();
-
-			for (var x in filters) {
-				if(t.match(x)) {
-					remove = score < filters[x];
-				}
-			}
-
-			console.log('remove: ' + remove + ' score:' + score);
-			return remove;
-	}
+	return remove;
+}
